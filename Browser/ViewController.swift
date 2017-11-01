14c35d94bc9cea3ab635869f8f6b3b73328b1440
Ammy <ammypandey@Ammys-MacBook-Air.local>
2017-11-01 17:31:19 +0530

    initial commit

--- Browser/ViewController.swift
+++ Browser/ViewController.swift
@@ -8,18 +8,58 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    // ui obj
+    @IBOutlet weak var webView: UIWebView!
+    @IBOutlet weak var textField: UITextField!
+    
+    
+    // first load func
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // load website
+        let url = URL(string: "http://www.google.com")!
+        let request = URLRequest(url: url)
+        webView.loadRequest(request)
+    
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // exec when go back is clicked
+    @IBAction func goBack_clicked(_ sender: AnyObject) {
+        webView.goBack()
+    }
+    
+    // exec when go forward is clicked
+    @IBAction func goForward_clicked(_ sender: AnyObject) {
+        webView.goForward()
     }
 
+    // exec when stop is clicked
+    @IBAction func stop_clicked(_ sender: AnyObject) {
+        webView.stopLoading()
+    }
+    
+    // exec when reload is clicked
+    @IBAction func reload_clicked(_ sender: AnyObject) {
+        webView.reload()
+    }
+    
+    // called when Return / GO button is pressed
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        // laods website from textfield
+        let url = URL(string: "http://" + textField.text!)!
+        let request = URLRequest(url: url)
+        webView.loadRequest(request)
+        
+        // removes keyboard
+        textField.endEditing(true)
+        
+        return true
+    }
+    
 
 }
 
